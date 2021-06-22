704a9e1f765d04dc386f651be78ea2288a72cb74
almir93 <64337737+almir93@users.noreply.github.com>
2021-06-22 00:43:05 -0400

    increased speed of erasingdelay

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -3,7 +3,7 @@ const cursorSpan = document.querySelector(".cursor");
 
 const textArray = ["resourceful.", "hardworking.", "creative.", " a DEVELOPER."];
 const typingDelay = 100;
-const erasingDelay = 200;
+const erasingDelay = 100;
 const newTextDelay = 200; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
